838aab751b6ed0783d160dbfe6ffdfe73940e922
fischmar000 <fischmar000@warren.k12.in.us>
2021-08-04 11:51:29 -0400

    Completed L1 and L2 Notes

--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,12 @@
 public class L2 {
 
     //add the main method header
+    public static void main(String[] args){
+        int balance = 15;
+        boolean debt=true;
+        double due=5.30;
+        System.out.println(balance);
+        System.out.println(debt);
+        System.out.println(due);
+    }
 }
